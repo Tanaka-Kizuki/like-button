4031a5893d39792c864b9e815be2afe563e9875a
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-01-01 21:18:50 +0900

    create PostModel

--- database/migrations/2020_12_30_150834_create_posts_table.php
+++ database/migrations/2020_12_30_150834_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostMigration extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePostMigration extends Migration
      */
     public function up()
     {
-        Schema::create('post_migration', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->memo();
+            $table->text('memo');
         });
     }
 
