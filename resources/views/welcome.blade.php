666a2422a3179a147ba3cb361a1480635b0d2021
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-01-08 23:52:32 +0900

    update view home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,13 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <div class="content">
+                <div class="title m-b-md">
+                    Like-Button
+                </div>
+
+                <div class="links">
+                @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -77,17 +83,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Like-Button
-                </div>
-
-                <div class="links">
-                    <a href="{{ url('/home') }}">Home</a>
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
+                @endif
                 </div>
             </div>
         </div>
